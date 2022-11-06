ead59a6d07cd572878046e40b19a7a893683e34f
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-11-06 20:43:17 -0300

    commit II

--- AulasJS/01-Aula/01-desafio.js
+++ AulasJS/01-Aula/01-desafio.js
@@ -4,19 +4,13 @@ Variáveis:
 1 - Preço do gasolina 
 2 - Gasto médio de gasolina do carro por KM
 3 - Distância em KM da viagem
-
 Imprima no console o valor que será gasto para realizar a viagem.
-
 */
 
-const precoGasolina = 5.79;
-const kmPorLitros = 17;
-const distanciaEmKm = 1580;
+const precoGasolina = 4.79;
+const kmPorLitros = 14;
+const distanciaEmKm = 200;
 
 const litrosConsumidos = distanciaEmKm / kmPorLitros;
 const valorGasto = litrosConsumidos * precoGasolina;
 console.log('R$ ' + valorGasto.toFixed(2));
-
-
-
- 
