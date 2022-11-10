f91d29fdc60e12532f263130c5e0073b341362f3
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-11-09 23:13:40 -0300

    done

--- AulasJS/03 - Exercicios/01-ListaExercicios.js
+++ AulasJS/03 - Exercicios/01-ListaExercicios.js
@@ -10,17 +10,27 @@ Classificação:
 - Média acima de 7, passou de semestre;
 */
 
-
-const nota1 = 5;
-const nota2 = 6;
-const nota3 = 7;
-
-const media = (nota1 + nota2 + nota3) / 3;
-
-if (media < 5) {
-    console.log('Reprovação, com nota ' + media.toFixed(1));
-} else if (media >=5 && media <= 7) {
-    console.log('Recuperação, com nota ' + media.toFixed(1));
-} else {
-    console.log('Passou de semestre, com nota ' + media.toFixed(1));
+function calcularMedia(nota1, nota2, nota3) {
+return (nota1 + nota2 + nota3) / 3;
 }
+
+function resultadoEscolar(media) {
+if (media < 5) {
+    return 'Reprovação, com nota ' + media.toFixed(1);
+} else if (media >=5 && media < 7) {
+    return 'Recuperação, com nota ' + media.toFixed(1);
+} else {
+    return 'Passou de semestre, com nota ' + media.toFixed(1);
+}
+}
+
+
+function main() {
+    const nota1 = 7;
+    const nota2 = 7;
+    const nota3 = 7;
+    const media = calcularMedia(nota1,nota2, nota3);
+    console.log(resultadoEscolar(media));
+    }
+
+    main();
