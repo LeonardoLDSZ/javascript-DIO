2149f7111a3269c4c65317138b25440e76ffb3c8
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2022-12-11 20:29:43 -0300

    Array class

---
+++ AulasJS/06 - Arrays/Exercicios - Array/01-exercicio.js
@@ -0,0 +1,8 @@
+// Crie um programa que dado um numero imprima  a sua tabuada //
+
+const tabuCinco = 8;
+
+for (let i = 0; i <= 10; i++) {
+console.log(tabuCinco * i);
+       
+}
---
+++ AulasJS/06 - Arrays/Exercicios - Array/02-exercicio.js
@@ -0,0 +1,9 @@
+//Crie um programa capaz de percorrer uma lista de numeros e imprima cada numero par encontrado
+
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+
+for (let i = 0; i < array.length; i++) {
+    
+    
+}
